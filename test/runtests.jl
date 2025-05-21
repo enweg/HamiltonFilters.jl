965d629ecd859d0f2dfada17845c7ad1c3be5164
Enrico Wegner <wegner1996@gmail.com>
2025-05-21 10:54:51 +0200

    testing by comparing to matlab

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
-using HamiltonFilter
+using HamiltonFilters
+using DataFrames 
+using CSV
 using Test
 
-@testset "HamiltonFilter.jl" begin
-    # Write your tests here.
+@testset "HamiltonFilters.jl" begin
+    log_gdp = DataFrame(CSV.File("./logGDPC1.csv"))
+    matlab_hfilter = DataFrame(CSV.File("./matlab_hfilter.csv"))
+
+    hfilter = HamiltonFilter(8, 4)
+    trend, cycle = filter(hfilter, log_gdp)
+
+    @test sum(isnan.(trend.GDPC1)) == sum(isnan.(matlab_hfilter.Trend))
+    @test sum(isnan.(trend.GDPC1)) == 11
+    @test isapprox(trend.GDPC1[12:end], matlab_hfilter.Trend[12:end]; atol=1e-6)
+    @test isapprox(cycle.GDPC1[12:end], matlab_hfilter.Cycle[12:end]; atol=1e-6)
 end
